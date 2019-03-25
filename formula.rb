f616843a52d7adc626bc4f3128d0212e42398ed8
Ivan Smetanin <smetanin23@yandex.ru>
2019-03-25 09:15:17 +0300

    Update formula.rb

--- formula.rb
+++ formula.rb
@@ -1,17 +1,17 @@
 class Mergepbx < Formula
-  desc "script for merging XCode project files in git"
-  homepage "https://github.com/ismetanin/mergepbx"
-  url "https://github.com/ismetanin/mergepbx/archive/v0.11.tar.gz"
-  sha256 "868d4c181ef4ab65819e3fd16c0a7d669e8efbe21220d11454f94757308eafb7"
+  desc "Merge XCode project files in git"
+  homepage "https://github.com/simonwagner/mergepbx"
+  url "https://github.com/simonwagner/mergepbx/archive/v0.10.tar.gz"
+  sha256 "1727ea75ffbd95426fe5d1d825bfcfb82dbea3dbc03e96f6d7d7ab2699c67572"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "868d4c181ef4ab65819e3fd16c0a7d669e8efbe21220d11454f94757308eafb7" => :mojave
-    sha256 "868d4c181ef4ab65819e3fd16c0a7d669e8efbe21220d11454f94757308eafb7" => :high_sierra
-    sha256 "868d4c181ef4ab65819e3fd16c0a7d669e8efbe21220d11454f94757308eafb7" => :sierra
-    sha256 "868d4c181ef4ab65819e3fd16c0a7d669e8efbe21220d11454f94757308eafb7" => :el_capitan
-    sha256 "868d4c181ef4ab65819e3fd16c0a7d669e8efbe21220d11454f94757308eafb7" => :yosemite
-    sha256 "868d4c181ef4ab65819e3fd16c0a7d669e8efbe21220d11454f94757308eafb7" => :mavericks
+    sha256 "8188a967c09aadd08807dc11cb6695ebc6def26026532d426fd3bf1880fdc591" => :mojave
+    sha256 "2e295ff99574735aa7d1bb5244f1207a1f6f2054c4ec9da322e9d0981bdd5dd3" => :high_sierra
+    sha256 "35a545aa5eb9b4d761134818b792f50e007d7bb6235fbbf54e7733a8e35d742e" => :sierra
+    sha256 "9330e987d0c93a73b9edfbc77f265fa225b058d36b9210c797fe02494d1a656f" => :el_capitan
+    sha256 "77c1ec431ae1a7cd6fb4b04376e14e8aa1f7399cf840e006caf69c0f88839a7e" => :yosemite
+    sha256 "690559c9a95577702180b53493822f2c6887d2896f27c26cdfe9f2cad506809e" => :mavericks
   end
 
   resource "dummy_base" do
